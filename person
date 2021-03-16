2a22dc989e390da0b91cf90134973f0decf40c0e
Nur Hakiki Damayanti <dmyntkk07@gmail.com>
2021-03-16 14:43:11 +0700

    week02

---
+++ person
@@ -0,0 +1,5 @@
+run:
+Student Name	: Nur Hakiki Damayanti
+Student ID	: E41202305
+Student Status	: Active
+BUILD SUCCESSFUL (total time: 2 seconds)
---
+++ variable.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wsibd_pertemuan2;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class variable {
+    public static void main(String[] args) {
+        // deklarasi variable benar
+        
+        boolean gameOver = false;
+        int students = 50;
+        double sales_tax;
+        short number1;
+        
+        // deklarasi variable salah
+        
+        int 2beOrNor2be;
+        float price index;
+        double lastyear'sPrice';
+        long class;
+        
+                
+        
+    }
+    
+}
