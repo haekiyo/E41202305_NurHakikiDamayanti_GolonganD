35406f90c6545f2225722c6e20fd52834120bb2e
Nur Hakiki Damayanti <dmyntkk07@gmail.com>
2021-03-23 19:29:42 +0700

    week3

---
+++ arraynama/src/arraynama/Arraynama.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arraynama;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Arraynama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String Name1 = "Nur";
+        String Name2 = "Hakiki";
+        String Name3 = "Damayanti";
+        
+        System.out.println("Nur\t" + "Hakiki\t" + "Damayanti");
+        
+    }
+    
+}
